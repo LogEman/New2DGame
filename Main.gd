fb41bd07da591afff318bc97e176a4a78b574ae0
LogEman <thelogankessler@gmail.com>
2021-04-24 23:25:09 -0500

    devTest 1.4.1 - Bug fixes & Pseudocode
    Fixed a few bugs and added some pseudocode

--- Main.gd
+++ Main.gd
@@ -1,36 +1,48 @@
 extends Node
 
-export (PackedScene) var Enemy
+#References to Player & Enemy Scenes
+export (PackedScene) var Enemy 
 export (PackedScene) var Player
 
+#References to clones of enemy & player
 var enemy
 var player
 
+#Reference for playerDied bool
 var playerDied = false
 
 func _ready():
+	#Starts RNG with random seed
 	randomize()
 	
+	#Chooses a random point to put enemy
 	$EnemyPath/EnemySpawnLocation.offset = randi()
 	
+	#Defines enemy & player to source
 	var enemy = Enemy.instance()
 	var player = Player.instance()
 	
+	#Adds enemy & player clones as children to main node
 	add_child(enemy)
 	add_child(player)
 	
+	#Sets x cord of enemy at random and y at 545
 	enemy.position.x = $EnemyPath/EnemySpawnLocation.position.x
 	enemy.position.y = 545
 	
+	#Puts player at start position
 	player.position.x = 155
 	player.position.y = 425
 	
+	#Connects the player death signal to the player_death function
 	player.connect("death", self, "player_death")
 	
+	#Detects if player dies
 func player_death() :
 	playerDied = true
 
 func _process(delta):
 	if(playerDied) :
+		#Restarts whole game; will replace later
 		get_tree().reload_current_scene()
 		playerDied = false
--- Player.gd
+++ Player.gd
@@ -1,4 +1,5 @@
 extends Area2D
+#References to variables
 var health = 100
 var speed = 400
 var damageStrength = 25.0
@@ -9,52 +10,70 @@ var playerWasHit
 var maxJump = 200
 var floorPosition = 423
 
+#Reference to death signal
 signal death
 
+#Bool references
 var isJumping = false
 var jumpDisabled = false
 
+#Detects if player is damaged/hit
+# warning-ignore:unused_argument
 func playerHit(area):
 	playerWasHit = true
 
+#Detects when player is no longer being damaged/hit
+# warning-ignore:unused_argument
 func playerNotHit(area):
 	playerWasHit = false
 		
 func _process(delta):
 	var velocity = Vector2() #Players movement vector
+	#Detects key presses and changes velocity based on them
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-		$PlayerSprite.flip_h = false
+		$PlayerSprite.flip_h = false #Makes player sprite unflipped
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-		$PlayerSprite.flip_h = true
+		$PlayerSprite.flip_h = true #Flips player sprite
 		
+	#Activates gravity if not jumping and above floor
 	if (position.y < floorPosition && !isJumping) :
 		velocity.y += 1
-		
+	
+	#Detects jump button press and makes sure player can't fly
 	if (Input.is_action_pressed("ui_up") && position.y > maxJump && !jumpDisabled):
 		velocity.y -= 1
 		isJumping = true
+		
+	#Temporarily disables jumping if player lets go of jump button mid-jump; prevents gravity cancelling
 	if (isJumping && !Input.is_action_pressed("ui_up") || position.y < maxJump) :
 		jumpDisabled = true
 		isJumping = false
+	#Detects when player is on the floor and allows them to jump again
 	if (position.y > floorPosition + 1) :
 		jumpDisabled = false
-	
+	#Corrects weird issues with velocity and plays walking animation
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$PlayerSprite.play()
 	else:
+		#Stops player sprite when not moving and resets the animation cycle
 		$PlayerSprite.stop()
-		
+		$PlayerSprite.set_frame(0)
+	#Translates player velocity to position
 	position += velocity * delta
-	
+	#Player damage system
 	if (playerWasHit == true) :
+		#Sets a timer adjusted for frame rate
 		damageTimerDelta = damageTimer / delta
+		#Counts up damage timer if not completed
 		if (currentDamageTimer <= damageTimerDelta) :
 			currentDamageTimer += 1
+		#Gives player damage if not dead
 		elif (health > 0.0) :
 			health = health - damageStrength
+		#Resets player when they die
 		elif (health <= 0.0):
 			hide()
 			position.x = 155
