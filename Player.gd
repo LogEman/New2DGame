bb78cc3cafc2c9f1c636bfed99a29fde9f85d21a
LogEman <thelogankessler@gmail.com>
2021-04-24 20:19:11 -0500

    devTest 1.3 - Jump Test
    Now with jumping!

--- Player.gd
+++ Player.gd
@@ -1,14 +1,16 @@
 extends Area2D
-
-
-var frames
-var speed = 400
 var health = 100
-var damageStrength = 25
+var speed = 400
+var damageStrength = 25.0
 var damageTimer = 20
 var damageTimerDelta = 0
 var currentDamageTimer = 0
 var playerWasHit
+var maxJump = 200
+var floorPosition = 424
+
+var isJumping = false
+var jumpDisabled = false
 
 func playerHit(area):
 	playerWasHit = true
@@ -21,21 +23,35 @@ func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 		$PlayerSprite.flip_h = false
-	if Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 		$PlayerSprite.flip_h = true
+		
+	if (position.y < floorPosition && !isJumping) :
+		velocity.y += 1
+		
+	if (Input.is_action_pressed("ui_up") && position.y > maxJump && !jumpDisabled):
+		velocity.y -= 1
+		isJumping = true
+	if (isJumping && !Input.is_action_pressed("ui_up") || position.y < maxJump) :
+		jumpDisabled = true
+		isJumping = false
+	if (position.y > floorPosition + 1) :
+		jumpDisabled = false
+	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$PlayerSprite.play()
 	else:
 		$PlayerSprite.stop()
+		
 	position += velocity * delta
+	
 	if (playerWasHit == true) :
-		damageTimerDelta = damageTimer * delta * 1000
+		damageTimerDelta = damageTimer / delta
 		if (currentDamageTimer <= damageTimerDelta) :
 			currentDamageTimer += 1
-		elif (health > 0) :
+		elif (health > 0.0) :
 			health = health - damageStrength
-		elif (health <= 0):
+		elif (health <= 0.0):
 			hide()
-			
